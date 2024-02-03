5d8501711a4e2c45b8a700c016073e06cd3a711a
mnn <mnn.saag@gmail.com>
2024-02-03 07:14:14 +0300

    first commit

---
+++ react-router/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from './components/Header/Header'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-blue-200 text-4xl text-center'>React Router</h1>
+      {/* <Header /> */}
+    </>
+  )
+}
+
+export default App
